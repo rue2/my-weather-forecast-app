1ef549ccd8045c98d659b1787836870e51c5fa37
rue2 <153995661+rue2@users.noreply.github.com>
2024-03-21 08:38:42 -0700

    injected the forecast HTML from JavaScript

--- src/index.js
+++ src/index.js
@@ -51,8 +51,43 @@ function handleSearchSubmit(event) {
 
   searchCity(searchInput.value);
 }
+function displayForecast () {
 
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+  days.forEach(function(day) {
+    forecastHtml= forecastHtml +
+    `
+    <div class="row">
+   <div class="col-2">
+     <div class="weather-forecast-date">
+       ${day}
+     </div>
+     
+   <img src="https://openweathermap.org/img/wn/50d@2x.png"
+    alt=""
+     width="42"/>
+     <div class="weather-forecast-temperature">
+       <span class="weather-forecast-temp-max">
+         18°
+       </span>
+       <span class="weather-forecast-temp-min">
+         12°
+       </span>
+     </div>
+    
+   </div>
+   </div>
+   `;
+ });
+
+ let forecastElement = document.querySelector("#forecast");
+ forecastElement.innerHTML = forecastHtml ;
+  };
+  
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Harare");
+displayForecast();
+
